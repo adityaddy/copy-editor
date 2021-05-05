f3c16a1a36231bea7bd9f935c076ee61dbc9e946
adityaaddy <aditya.k@students.iiit.ac.in>
2021-05-05 18:29:48 +0530

    Google Doc, Lorem Ipsum and other support added

--- app.jsx
+++ app.jsx
@@ -12,6 +12,45 @@ import styled from "styled-components";
 import Colors from "./constants/colors";
 import Spacing from "./constants/spacing";
 
+const getPropFromStyle = (style) => {
+    if (!style)
+        return "normal"
+    var properties = style.split(';')
+    var isItalic = false
+    try {
+        isItalic = !!style.match(/italic/);
+    } catch (error) {
+        // console.log(error)
+    }
+
+    var isBold = false
+    try {
+        for (const property of properties) {
+            var keyVal = property.split(':')
+            const key = keyVal[0]
+            const val = keyVal[1]
+            if (key.includes('weight')) {
+                if (val.includes('bold') || parseInt(val) >= 600) {
+                    console.log("Weight: ", val)
+                    isBold = true
+                }
+            }
+        }
+    } catch (error) {
+        // console.log(error)
+    }
+
+    if (isItalic && !isBold) {
+        return "italic";
+    } else if (!isItalic && isBold) {
+        return "bold"
+    } else if (isItalic && isBold) {
+        return "bold-italic"
+    } else {
+        return "normal"
+    }
+}
+
 const parseNodes = (nodes, baseStyle = "normal") => {
     let parsed = [];
     for (const node of nodes) {
@@ -21,14 +60,14 @@ const parseNodes = (nodes, baseStyle = "normal") => {
                 style: baseStyle,
                 content: data,
             });
-        } else if (name === "b") {
+        } else if (name === "b" || name === "strong") {
             parsed = parsed.concat(
                 parseNodes(
                     children,
                     baseStyle === "italic" ? "bold-italic" : "bold"
                 )
             );
-        } else if (name === "i") {
+        } else if (name === "i" || name === "em") {
             parsed = parsed.concat(
                 parseNodes(
                     children,
@@ -39,17 +78,36 @@ const parseNodes = (nodes, baseStyle = "normal") => {
             const { style } = attribs;
             // The detection of attributes here might be too specific. Is this
             // really the best way to do this?
-            const isItalic = !!style.match(/italic/);
-            const isBold = !!style.match(/weight:600/);
-            if (isItalic && !isBold) {
-                parsed = parsed.concat(parseNodes(children, "italic"));
-            } else if (!isItalic && isBold) {
-                parsed = parsed.concat(parseNodes(children, "bold"));
-            } else if (isItalic && isBold) {
-                parsed = parsed.concat(parseNodes(children, "bold-italic"));
-            } else {
-                parsed = parsed.concat(parseNodes(children, "normal"));
-            }
+
+            const prop = getPropFromStyle(attribs.style)
+            parsed = parsed.concat(parseNodes(children, prop));
+            // var isItalic = false
+            // try {
+            //   isItalic = !!style.match(/italic/);  
+            // } catch (error) {
+            //   // console.log(error)
+            // } 
+
+            // var isBold = false
+            // try {
+            //   isBold = !!style.match(/weight:600/);
+
+            // } catch (error) {
+            //   // console.log(error)
+            // }
+            // if (isItalic && !isBold) {
+            //     parsed = parsed.concat(parseNodes(children, "italic"));
+            // } else if (!isItalic && isBold) {
+            //     parsed = parsed.concat(parseNodes(children, "bold"));
+            // } else if (isItalic && isBold) {
+            //     parsed = parsed.concat(parseNodes(children, "bold-italic"));
+            // } else {
+            //     parsed = parsed.concat(parseNodes(children, "normal"));
+            // }
+        } else {
+            const prop = getPropFromStyle(attribs.style)
+            parsed = parsed.concat(parseNodes(children, prop));
+
         }
     }
     return parsed;
@@ -63,8 +121,8 @@ const parseHtml = (html) =>
                 const parsed = parseNodes(children);
                 return parsed.length > 0
                     ? {
-                          content: parsed,
-                      }
+                        content: parsed,
+                    }
                     : null;
             } else {
                 return null;
@@ -81,7 +139,7 @@ const App = () => {
     };
 
     React.useEffect(() => {
-        const parsedHtml = parseHtml(html);
+        const parsedHtml = parseHtml("<div>" + html + "</div>");
         setParsed(parsedHtml);
     }, [html]);
 
